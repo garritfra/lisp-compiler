1098f84e69a76382e21df9c7c933c8a916c35295
frankeg <garrit.franke@ckc.de>
2019-03-30 20:06:17 +0100

    Add mock CLI

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "lisp> "
+});
+
+rl.prompt();
+
+rl.on("line", line => {
+  evaluate(line.trim());
+  rl.prompt();
+});
+
+function evaluate(input) {
+  console.log(input);
+}
